b79450f4eec159e89f77783df7ce023cef7f7d74
Oliver Valls <oliver.vh@coditramuntana.com>
2020-07-06 16:27:10 +0200

    Reconfigure custom_csv_census initializer

--- config/initializers/decidim_verifications_custom_csv_census.rb
+++ config/initializers/decidim_verifications_custom_csv_census.rb
@@ -2,15 +2,31 @@
 
 Decidim::Verifications::CustomCsvCensus.configure do |config|
   config.fields = {
-    id_document: {
+    # NIF de l'afa/ampa
+    nif_afa: {
       type: String,
       search: true,
       format: /\A[A-Z0-9]*\z/
     },
+    # N. de sòcia
+    membership_number: {
+      type: String,
+      search: true,
+      # format: /\A[A-Z0-9]*\z/
+    },
+    # DNI
     nif_document: {
       type: String,
       search: true,
       format: /\A[A-Z0-9]*\z/
+    },
+    # data de naixement de la persona designada per l'AMPA/AFA per assistir a l'assemblea.
+    # format dd/mm/yyyy
+    birth_date: {
+      type: Date,
+      search: true,
+      format: %r{\d{2}\/\d{2}\/\d{4}},
+      parse: proc { |s| s.to_date }
     }
   }
 end
