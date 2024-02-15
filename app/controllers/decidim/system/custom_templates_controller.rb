5a93c642fc065f083764af65a085bf2afd0136b6
elviabth <ejbenedith@gmail.com>
2024-02-15 16:44:08 +0100

    create ensure_template_exist method

--- app/controllers/decidim/system/custom_templates_controller.rb
+++ app/controllers/decidim/system/custom_templates_controller.rb
@@ -3,7 +3,17 @@
 module Decidim
   module System
     class CustomTemplatesController < Decidim::System::ApplicationController
+      before_action :ensure_template_exists, only: [:new]
       def new; end
+
+      private
+
+      def ensure_template_exists
+        return if params[:template].present?
+
+        flash.alert = I18n.t("decidim.system.custom_templates.no_template")
+        redirect_to decidim_system.root_path
+      end
     end
   end
 end
