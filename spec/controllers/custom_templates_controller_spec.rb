172c98afbcc9a4adb9cb6869fdb01b4baa99681a
elviabth <ejbenedith@gmail.com>
2024-02-19 14:19:58 +0100

    add system test for organization

---
+++ spec/controllers/custom_templates_controller_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+# module Decidim::System
+#   describe CustomTemplatesController, type: :controller do
+#     routes { Decidim::System::Engine.routes }
+#   end
+# end
--- spec/system/organizations_spec.rb
+++ spec/system/organizations_spec.rb
@@ -16,5 +16,20 @@ describe "Organizations", type: :system do
       expect(page).to have_content("Consultation for aFFas")
       expect(page).to have_content("New from template")
     end
+
+    it "shows templates available to create an organization" do
+      select "Second", from: "customTemplateSelect"
+      click_link "New from template"
+
+      expect(page).to have_content("Organization form")
+
+      fill_in "Name", with: "Citizen Corp"
+      fill_in "Host", with: "www.example.org"
+      fill_in "Organization admin name", with: "City Mayor"
+      fill_in "Organization admin email", with: "mayor@example.org"
+      click_button "Create Organization"
+
+      expect(page).to have_css("div.flash.alert")
+    end
   end
 end
