e986319354554b6702ae8a125ad1433b3ebda98b
elviabth <ejbenedith@gmail.com>
2024-02-20 13:49:01 +0100

    list templates files content

--- app/services/organization_templates.rb
+++ app/services/organization_templates.rb
@@ -1,6 +1,15 @@
+# frozen_string_literal: true
+
 class OrganizationTemplates
   def self.all
-  	# listar plantillas en lib/templates/*.yml
-  	# para cada plantilla, leer el YAML y devolver un hash con el nombre y el id
+    template_files = Dir.glob("lib/templates/*.yml")
+
+    templates = []
+    template_files.each do |file|
+      content = YAML.load_file(file)
+      templates << { "name" => content["name"], "id" => content["id"] }
+    end
+
+    templates
   end
 end
--- spec/services/organization_templates_spec.rb
+++ spec/services/organization_templates_spec.rb
@@ -4,8 +4,10 @@ require "rails_helper"
 
 describe OrganizationTemplates do
   it "list all organization templates" do
-	expect(OrganizationTemplates.all).to eq([
-	  { "name" => "Votacions AFFaC", "id" => "affac-votings" }
-	])
+    expect(OrganizationTemplates.all).to eq([
+                                              { "name" => "Votacions AFFaC", "id" => "affac-votings" },
+                                              { "name" => "Husker-aFFaC", "id" => "husker" },
+                                              { "name" => "Second", "id" => "two" }
+                                            ])
   end
 end
