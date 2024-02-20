6e11d1eae1447980d3d007d2a1ef6c7add9134a3
Ivan Vergés <ivan@pokecode.net>
2024-02-20 12:11:08 +0100

    initial scafolding

--- app/services/organization_templates.rb
+++ app/services/organization_templates.rb
@@ -1,4 +1,6 @@
 class OrganizationTemplates
-
-    
+  def self.all
+  	# listar plantillas en lib/templates/*.yml
+  	# para cada plantilla, leer el YAML y devolver un hash con el nombre y el id
+  end
 end
--- spec/services/organization_templates_spec.rb
+++ spec/services/organization_templates_spec.rb
@@ -3,5 +3,9 @@
 require "rails_helper"
 
 describe OrganizationTemplates do
-    
+  it "list all organization templates" do
+	expect(OrganizationTemplates.all).to eq([
+	  { "name" => "Votacions AFFaC", "id" => "affac-votings" }
+	])
+  end
 end
