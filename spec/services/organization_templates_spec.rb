f1cf5666d5ff449b5690f432466265134504a08b
elviabth <ejbenedith@gmail.com>
2024-02-26 15:39:13 +0100

    fix organization_templates spec

--- spec/services/organization_templates_spec.rb
+++ spec/services/organization_templates_spec.rb
@@ -4,6 +4,7 @@ require "rails_helper"
 
 describe OrganizationTemplates do
   it "list all organization templates" do
+    OrganizationTemplates.template_root = "spec/fixtures/templates"
     expect(OrganizationTemplates.all).to eq([
                                               { "name" => "Votacions AFFaC", "id" => "affac-votings" },
                                               { "name" => "Husker-aFFaC", "id" => "husker" },
