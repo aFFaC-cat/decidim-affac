8ca6a4180137372c504e24e4cb1e048fba3dd5a7
elviabth <ejbenedith@gmail.com>
2024-02-27 18:18:34 +0100

    ad more checks to forms spec

--- spec/forms/register_custom_templates_form_spec.rb
+++ spec/forms/register_custom_templates_form_spec.rb
@@ -4,33 +4,67 @@ require "rails_helper"
 
 module Decidim::System
   describe RegisterCustomTemplatesForm do
-    let(:name) { Decidim::Faker::Localized.word }
-    let(:host) { Decidim::Faker::Localized.word }
-    let(:organization_admin_email) { Decidim::Faker::Localized.email }
-    let(:organization_admin_name) { Decidim::Faker::Localized.word }
+    context "with valid attributes" do
+      let(:name) { Decidim::Faker::Localized.name }
+      let(:host) { Decidim::Faker::Localized.word }
+      let(:reference_prefix) { Decidim::Faker::Localized.word }
+      let(:organization_admin_email) { Faker::Internet.email }
+      let(:organization_admin_name) { Decidim::Faker::Localized.name }
 
-    context "when name is missing" do
-      let(:name) { {} }
-
-      it { is_expected.to be_invalid }
+      it { is_expected.to be_valid }
     end
 
-    context "when host is missing" do
-      let(:host) { {} }
+    context "with missing attributes" do
+      shared_examples "invalid form" do
+        it { is_expected.to be_invalid }
+      end
 
-      it { is_expected.to be_invalid }
+      context "when name is missing" do
+        let(:name) { {} }
+
+        it_behaves_like "invalid form"
+      end
+
+      context "when host is missing" do
+        let(:host) { {} }
+
+        it_behaves_like "invalid form"
+      end
+
+      context "when organization admin email is missing" do
+        let(:organization_admin_email) { {} }
+
+        it_behaves_like "invalid form"
+      end
+
+      context "when organization admin name is missing" do
+        let(:organization_admin_name) { {} }
+
+        it_behaves_like "invalid form"
+      end
     end
 
-    context "when organization admin email is missing" do
-      let(:organization_admin_email) { {} }
+    describe "#default_locale" do
+      context "when template has a default_locale" do
+        let(:template) { instance_double(OrganizationTemplates, fields: { "organization" => { "default_locale" => "es" } }) }
 
-      it { is_expected.to be_invalid }
-    end
+        before { allow(subject).to receive(:template).and_return(template) }
 
-    context "when organization admin name is missing" do
-      let(:organization_admin_name) { {} }
+        it "returns the default_locale from the template" do
+          expect(subject.default_locale).to eq("es")
+        end
+      end
 
-      it { is_expected.to be_invalid }
+      context "when template does not have a default_locale" do
+        let(:template) { instance_double(OrganizationTemplates, fields: {}) }
+
+        before { allow(subject).to receive(:template).and_return(template) }
+
+        it "returns the default_locale from Decidim" do
+          allow(Decidim).to receive(:default_locale).and_return("en")
+          expect(subject.default_locale).to eq("en")
+        end
+      end
     end
   end
 end
