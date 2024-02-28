9cadba8e29709fa0755b83941657c3c697fc843f
elviabth <ejbenedith@gmail.com>
2024-02-28 10:06:42 +0100

    fix register_custom_templates_form spec

--- spec/forms/register_custom_templates_form_spec.rb
+++ spec/forms/register_custom_templates_form_spec.rb
@@ -4,13 +4,34 @@ require "rails_helper"
 
 module Decidim::System
   describe RegisterCustomTemplatesForm do
-    context "with valid attributes" do
-      let(:name) { Decidim::Faker::Localized.name }
-      let(:host) { Decidim::Faker::Localized.word }
-      let(:reference_prefix) { Decidim::Faker::Localized.word }
-      let(:organization_admin_email) { Faker::Internet.email }
-      let(:organization_admin_name) { Decidim::Faker::Localized.name }
+    let(:template_id) { "affac-votings" }
 
+    let(:name) { Decidim::Faker::Localized.name }
+    let(:host) { Decidim::Faker::Localized.word }
+    let(:reference_prefix) { Decidim::Faker::Localized.word }
+    let(:organization_admin_email) { Faker::Internet.email }
+    let(:organization_admin_name) { Decidim::Faker::Localized.name }
+
+    subject do
+      described_class.new(
+        template_id: template_id,
+        organization_admin_email: organization_admin_email,
+        organization_admin_name: organization_admin_name,
+        name: name,
+        reference_prefix: "TEST",
+        host: host
+      )
+    end
+
+    before do
+      OrganizationTemplates.template_root = "lib/templates"
+      template_content = {
+        "id" => template_id
+      }
+      allow(YAML).to receive(:load_file).and_return(template_content)
+    end
+
+    context "with valid attributes" do
       it { is_expected.to be_valid }
     end
 
@@ -20,25 +41,25 @@ module Decidim::System
       end
 
       context "when name is missing" do
-        let(:name) { {} }
+        let(:name) { nil }
 
         it_behaves_like "invalid form"
       end
 
       context "when host is missing" do
-        let(:host) { {} }
+        let(:host) { nil }
 
         it_behaves_like "invalid form"
       end
 
       context "when organization admin email is missing" do
-        let(:organization_admin_email) { {} }
+        let(:organization_admin_email) { nil }
 
         it_behaves_like "invalid form"
       end
 
       context "when organization admin name is missing" do
-        let(:organization_admin_name) { {} }
+        let(:organization_admin_name) { nil }
 
         it_behaves_like "invalid form"
       end
